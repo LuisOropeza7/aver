28723368c730324cfa131b1e9e8ab854012f2a75
Luis Oropeza <luisoropeza113@gmail.com>
2021-08-06 17:14:46 -0500

    Changes to be committed: 	modified:   otros.sh

--- otros.sh
+++ otros.sh
@@ -1,3 +1,6 @@
-#!/usr/bin/fish
+#!/usr/bin/bash
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm 
 
-dnf install neovim nodejs texlive-scheme-full wine lutris steam -y
+dnf install fish neovim nodejs texlive-scheme-full wine lutris steam 
+
+chsh -s /usr/bin/fish
